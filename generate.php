14a3f7b8068693c86d390fce8dd2b8d701cee30f
Rémi <rmikel@clever-age.com>
2021-12-02 09:07:47 +0100

    new set

--- generate.php
+++ generate.php
@@ -1,4 +1,12 @@
 <?php
+
+
+// film MAx : 52
+// hastag max 12
+// Gros mots : 25
+//Livre : 52
+// TODO Si pas d'espace découper la chaine
+
 function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct)
 {
     // creating a cut resource
@@ -13,19 +21,154 @@ function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, 
     // insert cut resource to destination image
     imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
 }
+function wrap($fontSize, $angle, $fontFace, $string, $width){
+
+    $ret = "";
+
+    $arr = explode(' ', $string);
+
+    foreach ( $arr as $word ){
+
+        $teststring = $ret.' '.$word;
+        $testbox = imagettfbbox($fontSize, $angle, $fontFace, $teststring);
+        if ( $testbox[2] > $width ){
+            $ret.=($ret==""?"":"\n").$word;
+        } else {
+            $ret.=($ret==""?"":' ').$word;
+        }
+    }
+
+    return $ret;
+}
 // Création des instances d'image
 $dest = imagecreatefromjpeg('./images/full.jpg');
 
+$primary = imagecolorallocate($dest, 0, 0, 0);
+$fontAmatic = './fonts/AmaticSC-Bold.ttf';
+$fontQuick = './fonts/Quicksand-Bold.ttf';
+
+
+
 // // Copie et fusionne
 //Animaux
-$src = imagecreatefrompng('./images/animaux/Animal_0009_Ecureuil.png');
+$src = imagecreatefrompng('./images/JAUNE/animaux/ecureuil.png');
 imagecopymerge_alpha($dest, $src, 830, 1105, 0, 0, 921, 925, 100);
 
 //Boisson
-$src = imagecreatefrompng('./images/boissons/Boisson_0002_Cafe-.png');
+$src = imagecreatefrompng('./images/JAUNE/boissons/cafe.png');
 imagecopymerge_alpha($dest, $src, 1737, 290, 0, 0, 577, 1101, 100);
 
+//Dessert
+$src = imagecreatefrompng('./images/JAUNE/desserts/cannele.png');
+imagecopymerge_alpha($dest, $src, 690, 2685, 0, 0, 738, 623, 100);
 
+//chiffres
+$src = imagecreatefrompng('./images/JAUNE/chiffres/9.png');
+imagecopymerge_alpha($dest, $src, 370, 2935, 0, 0, 326, 375, 100);
+
+//plante
+$src = imagecreatefrompng('./images/JAUNE/plantes1/ginkgo.png');
+imagecopymerge_alpha($dest, $src, 10, 895, 0, 0, 1215, 210, 100);
+$src = imagecreatefrompng('./images/JAUNE/plantes2/gingko.png');
+imagecopymerge_alpha($dest, $src, 6, 1657, 0, 0, 496, 554, 100);
+$src = imagecreatefrompng('./images/JAUNE/plantes3/ginkgo.png');
+imagecopymerge_alpha($dest, $src, 1750, 1385, 0, 0, 573, 380, 100);
+
+//sport
+$src = imagecreatefrompng('./images/JAUNE/sport1/tennis.png');
+imagecopymerge_alpha($dest, $src, 805, 300, 0, 0, 1091, 811, 100);
+$src = imagecreatefrompng('./images/JAUNE/sport2/tennis.png');
+$src = imagescale($src, 230);
+imagecopymerge_alpha($dest, $src, 0, 2057, 0, 0, 230, 368, 100);
+
+//chiffres
+$src = imagecreatefrompng('./images/JAUNE/transports/velo.png');
+imagecopymerge_alpha($dest, $src, 1420, 2800, 0, 0, 892, 504, 100);
+
+
+//Film
+$src = imagecreatefrompng('./images/JAUNE/cinema/film.png');
+imagecopymerge_alpha($dest, $src, 0, 0, 0, 0, 1457, 1102, 100);
+$title = "Eternalit";
+if (strlen($title) >= 52){
+    imagettftext($dest, 60, 6, 120, 370, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 610));
+}
+else if (strlen($title) >= 35){
+    imagettftext($dest, 70, 6, 120, 380, $primary, $fontAmatic, wrap(70, 6, $fontAmatic, $title, 610));
+}
+else if (strlen($title) >= 20){
+    imagettftext($dest, 90, 6, 120, 420, $primary, $fontAmatic, wrap(90, 6, $fontAmatic, $title, 610));
+}
+else if (strlen($title) >= 10) {
+    imagettftext($dest, 100, 6, 125, 470, $primary, $fontAmatic, $title);
+}
+else {
+    imagettftext($dest, 150, 6, 165, 470, $primary, $fontAmatic, $title);
+}
+
+//gros mot
+$src = imagecreatefrompng('./images/JAUNE/gros_mot/gros_mot.png');
+imagecopymerge_alpha($dest, $src, 0, 1100, 0, 0, 971, 557, 100);
+$title = "Putain !";
+if (strlen($title) >= 20){
+    imagettftext($dest, 90, 6, 120, 1380, $primary, $fontAmatic, wrap(90, 6, $fontAmatic, $title, 610));
+}
+else if (strlen($title) >= 10) {
+    imagettftext($dest, 100, 6, 70, 1470, $primary, $fontAmatic, $title);
+}
+else {
+    imagettftext($dest, 170, 6, 180, 1490, $primary, $fontAmatic, $title);
+}
+
+//Livre
+$src = imagecreatefrompng('./images/JAUNE/livre/livre.png');
+imagecopymerge_alpha($dest, $src, 1220, 1770, 0, 0, 744, 1033, 100);
+$title = "ABCDEFGHJ";
+if (strlen($title) >= 52){
+    imagettftext($dest, 60, 21, 1420, 2300, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 400));
+}
+else if (strlen($title) >= 35){
+    imagettftext($dest, 65, 21, 1420, 2300, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 400));
+}
+else if (strlen($title) >= 20){
+    imagettftext($dest, 75, 21, 1420, 2300, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 300));
+}
+else if (strlen($title) >= 10) {
+    imagettftext($dest, 75, 21, 1400, 2300, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 300));
+}
+else {
+    imagettftext($dest, 85, 21, 1380, 2300, $primary, $fontAmatic, $title);
+}
+
+//Musique
+$src = imagecreatefrompng('./images/JAUNE/musique/musique.png');
+imagecopymerge_alpha($dest, $src, 1220, 1770, 0, 0, 1213, 1647, 100);
+$title = "J'aime bien me mettre de la crème sur le visage";
+if (strlen($title) >= 35){
+    imagettftext($dest, 80, 15, 620, 2400, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 500));
+}
+else if (strlen($title) >= 20){
+    imagettftext($dest, 75, 21, 1420, 2300, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 300));
+}
+else if (strlen($title) >= 10) {
+    imagettftext($dest, 75, 21, 1400, 2300, $primary, $fontAmatic, wrap(60, 6, $fontAmatic, $title, 300));
+}
+else {
+    imagettftext($dest, 85, 21, 1380, 2300, $primary, $fontAmatic, $title);
+}
+
+// Date
+imagettftext($dest, 180, 0, 1400, 240, $primary, $fontQuick, "88.88.88");
+
+// Hashtag
+$hashtag = "#" . "HASHTAG";
+if (strlen($hashtag) <= 6) {
+    imagettftext($dest, 300, 90, 2280, 2600, $primary, $fontAmatic, $hashtag);
+} else if (strlen($hashtag) <= 9){
+    imagettftext($dest, 200, 90, 2230, 2620, $primary, $fontAmatic, $hashtag);
+}else if (strlen($hashtag) <= 12){
+    imagettftext($dest, 150, 90, 2200, 2620, $primary, $fontAmatic, $hashtag);
+}
 
 
 // Affichage et libération de la mémoire
--- index.php
+++ index.php
@@ -1,130 +1,21 @@
 <?php include 'header.php'; ?>
 <div style="margin-left:auto;margin-right:auto;text-align:center">
     <h1>
-        Félicitations tu as trouvé le code des énigmes !
+    Tu as trouvé la solution des énigmes de la Bougie Mystère, tu vas maintenant découvrir ta surprise...
     </h1>
     <p>
-        J'aimerais en savoir tellement plus sur toi car le vrai trésor se trouve en soi ! Mais t'inquiètes pas j'ai bien quelque chose pour toi !
+    La Bougie Mystère est née de l’imagination de Noémie et de Rémi, nous espérons que l’expérience t’aura plu !
+Noémie est illustratrice, Rémi est développeur web, et en mixant leurs compétences on obtient...un champ des possibles immense pour te créer une
+illustration de manière automatisée et en même temps complètement sur-mesure. Impossible tu dis ?
     </p>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <p>
+    On te laisse remplir le formulaire ci-dessous, à la manière d’un portrait chinois, il s’agit de faire un « Arrêt sur Image » de ta vie à ce moment précis
+avant de générer ton illustration personnalisée en format carte postale. Tu pourras d’ailleurs réitérer l’expérience quand tu le souhaites, pour toi ou
 
-    <script>
-        $(document).ready(function() {
-
-            $("#btn_recherche").click(function() {
-                var statut = $(this).attr("statut");
-                $("#bloc_recherche_couleur").slideToggle("slow");
-                if (statut == "ferme") {
-                    $(this).attr('statut', "ouvert");
-                    $(this).html('Fermer le questionnaire');
-                } else {
-                    $(this).attr('statut', "ferme");
-                    $(this).html('On commence ?');
-                }
-            });
-
-
-            $(".etape_suivante").click(function() {
-                var etape_actuelle = $(this).attr('etape');
-                var etape_suivante = parseInt(etape_actuelle) + 1;
-                var curHeight = $("#etape" + etape_actuelle + "_recherche").height();
-                var autoHeight = $("#etape" + etape_suivante + "_recherche").height();
-                $("#etape" + etape_actuelle + "_recherche").hide("slide", {
-                    direction: "up"
-                }, 300, function() {
-                    $('#bloc_recherche_couleur').height(curHeight + 40).animate({
-                        height: autoHeight + 40
-                    }, 300, function() {
-
-                        $("#etape" + etape_suivante + "_recherche").show("slide", {
-                            direction: "down"
-                        }, 300);
-                    });
-                });
-            });
-
-            $(".etape_precedente").click(function() {
-                var etape_actuelle = $(this).attr('etape');
-                var etape_precedent = parseInt(etape_actuelle) - 1;
-                var curHeight = $("#etape" + etape_actuelle + "_recherche").height();
-                var autoHeight = $("#etape" + etape_precedent + "_recherche").height();
-                $("#etape" + etape_actuelle + "_recherche").hide("slide", {
-                    direction: "down"
-                }, 300, function() {
-                    $('#bloc_recherche_couleur').height(curHeight + 40).animate({
-                        height: autoHeight + 40
-                    }, 300, function() {
-
-                        $("#etape" + etape_precedent + "_recherche").show("slide", {
-                            direction: "up"
-                        }, 300);
-                    });
-
-                });
-            });
-
-
-            $("#terminer").click(function() {
-
-                //HERE SUBMIT THE FORM WITH AN AJAX REQUEST (serialize #myform's inputs, etc...)...
-
-                $("#bloc_recherche_couleur").slideToggle("slow");
-                $("#btn_recherche").html('OPEN THE QUIZ');
-                $(".div_cache").hide();
-                $(".champtxt").val('');
-                $("#etape0_recherche").show(1500);
-            });
-        });
-    </script>
-    <style>
-
-        .titre {
-            color: #ffffff;
-            font-size: 24px;
-        }
-
-        .btn_noir {
-            font-size: 14px;
-            cursor: pointer;
-            padding: 8px;
-            border-radius: 2px;
-            background-color: #333333;
-            color: #ffffff;
-            display: inline-block;
-            margin: 8px;
-            font-weight: 500;
-            width: 100px;
-        }
-
-        .btn_noir:hover {
-            background-color: #000000;
-        }
-
-        /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! RECHERCHE !!!!!!!!!!!!!!!!!!!!!!!!!! */
-
-        #bloc_recherche_couleur {
-            background-color: #ffbe40;
-            width: 100%;
-            height: 200px;
-            display: none;
-        }
-
-
-        #btn_recherche {
-            background-color: #ffbe40;
-            width: 400px;
-            margin-bottom: 20px;
-            color: #ffffff;
-            font-weight: 500;
-            text-align: center;
-            padding: 10px;
-            border-radius: 0px 0px 3px 3px;
-            cursor: pointer;
-        }
-    </style>
+même pour faire plaisir à quelqu’un d’autre !
+    </p>
     <div align="center">
-        <form id="myform">
+        <form id="myform" action="generate.php" method="POST">
             <div id="bloc_recherche_couleur">
 
                 <div id="etape0_recherche" style="color:#ffffff;"><br />
@@ -136,7 +27,7 @@
 
                 <div id="etape1_recherche" class="div_cache" style="display:none; text-align:center;"><br />
                     <div class="titre">C'est quelle chanson que t'écoutes le plus ces jours-ci ?</div><br />
-                    <input required type="text" name="chason" value="" placeholder="" style="width:300px; font-size:16px; padding:5px; margin:0px;" class="champtxt" /><br /><br />
+                    <input required type="text" name="chanson" value="" placeholder="" style="width:300px; font-size:16px; padding:5px; margin:0px;" class="champtxt" /><br /><br />
 
                     <div class="titre">Tu te sens comme quel animal aujourd'hui ?</div><br />
                     <select name="animaux" id="pet-select"  style="width:300px; font-size:16px; padding:5px; margin:0px;" class="champtxt" required>
@@ -171,6 +62,8 @@
                     <br><br>
                     <div class="btn_noir etape_precedente" etape="1">Précédente</div>
                     <div class="btn_noir etape_suivante" etape="1">Suivante</div>
+
+                    <input type="submit"  class="btn_noir" value="Générer l'image"></input>
                 </div>
 
                 <div id="etape2_recherche" class="div_cache" style="display:none; text-align:center;"><br />
@@ -193,7 +86,6 @@
                     <div class="titre">Thanks ! </div><br />
                     Now I know you...<br /> <br />
                     <div class="btn_noir etape_precedente" etape="4">Précédente</div>
-                    <div class="btn_noir" id="terminer">SAVE &amp; Suivante</div>
                 </div>
 
 
@@ -202,4 +94,111 @@
         </form>
     </div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+
+        $("#btn_recherche").click(function() {
+            var statut = $(this).attr("statut");
+            $("#bloc_recherche_couleur").slideToggle("slow");
+            if (statut == "ferme") {
+                $(this).attr('statut', "ouvert");
+                $(this).html('Fermer le questionnaire');
+            } else {
+                $(this).attr('statut', "ferme");
+                $(this).html('On commence ?');
+            }
+        });
+
+
+        $(".etape_suivante").click(function() {
+            var etape_actuelle = $(this).attr('etape');
+            var etape_suivante = parseInt(etape_actuelle) + 1;
+            var curHeight = $("#etape" + etape_actuelle + "_recherche").height();
+            var autoHeight = $("#etape" + etape_suivante + "_recherche").height();
+            $("#etape" + etape_actuelle + "_recherche").hide("slide", {
+                direction: "up"
+            }, 300, function() {
+                $('#bloc_recherche_couleur').height(curHeight + 40).animate({
+                    height: autoHeight + 40
+                }, 300, function() {
+
+                    $("#etape" + etape_suivante + "_recherche").show("slide", {
+                        direction: "down"
+                    }, 300);
+                });
+            });
+        });
+
+        $(".etape_precedente").click(function() {
+            var etape_actuelle = $(this).attr('etape');
+            var etape_precedent = parseInt(etape_actuelle) - 1;
+            var curHeight = $("#etape" + etape_actuelle + "_recherche").height();
+            var autoHeight = $("#etape" + etape_precedent + "_recherche").height();
+            $("#etape" + etape_actuelle + "_recherche").hide("slide", {
+                direction: "down"
+            }, 300, function() {
+                $('#bloc_recherche_couleur').height(curHeight + 40).animate({
+                    height: autoHeight + 40
+                }, 300, function() {
+
+                    $("#etape" + etape_precedent + "_recherche").show("slide", {
+                        direction: "up"
+                    }, 300);
+                });
+
+            });
+        });
+
+    });
+</script>
+<style>
+
+    .titre {
+        color: #ffffff;
+        font-size: 24px;
+    }
+
+    .btn_noir {
+        font-size: 14px;
+        cursor: pointer;
+        padding: 8px;
+        border-radius: 2px;
+        background-color: #333333;
+        color: #ffffff;
+        display: inline-block;
+        margin: 8px;
+        font-weight: 500;
+        width: 100px;
+    }
+
+    .btn_noir:hover {
+        background-color: #000000;
+    }
+
+    /* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! RECHERCHE !!!!!!!!!!!!!!!!!!!!!!!!!! */
+
+    #bloc_recherche_couleur {
+        background-color: #ffbe40;
+        width: 100%;
+        height: 200px;
+        display: none;
+    }
+
+
+    #btn_recherche {
+        background-color: #ffbe40;
+        width: 400px;
+        margin-bottom: 20px;
+        color: #ffffff;
+        font-weight: 500;
+        text-align: center;
+        padding: 10px;
+        border-radius: 0px 0px 3px 3px;
+        cursor: pointer;
+    }
+</style>
 <?php include 'footer.php'; ?>
